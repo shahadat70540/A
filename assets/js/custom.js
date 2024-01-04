330a24e8d4f175226eb8ecfff8e186e07a037a82
MD Shahadat Hossain <shsahdathossain@gmail.com>
2024-01-04 14:03:42 +0600

    About Section Start

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,4 +8,43 @@ $(document).ready(function(){
         closeOnClick: true,
     });
     // THE HEADER SECTION NAVIGATION SLICKNAV ENDS HERE
+    // THIS IS THE SOCIL MEDIA SHARE SECTION START HERE
+    $('.share-bar').share({
+        pageTitle:'',
+        pageDesc:'',
+        position:'right',
+        theme:'circle',
+        channels: ['facebook', 'twitter', 'linkedin', 'email', 'pinterest',],
+        popupWidth: 640,
+        popupHeight: 528,
+        animation: true,
+        itemTriggerClass:'js-share',
+    });
+    // THE SOCIL MEDIA SHARE SECTION ENDS HERE
+    // THIS IS THE ABOUT SECTION PROGRESBAR START HERE
+    $('.pogress_bar_1').circleProgress({
+        value: 1,
+        size: 140,
+        fill: {
+        gradient: ["#F70000", "rgb(22 55 238 )"]
+        },
+        animation: true,
+      });
+    $('.pogress_bar_2').circleProgress({
+        value: 1,
+        size: 140,
+        fill: {
+        gradient: ["rgb(22 55 238 )", "#F70000"]
+        },
+        animation: true,
+      });
+    $('.pogress_bar_3').circleProgress({
+        value: 1,
+        size: 140,
+        fill: {
+        gradient: ["#F70000", "rgb(22 55 238 )"]
+        },
+        animation: true,
+      });
+    // THE ABOUT SECTION PROGRESBAR ENDS HERE
 });
